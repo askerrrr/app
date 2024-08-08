cea16be8281f2b501d25db7e385d145c18df04c1
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-08-08 16:36:56 +0300

    change file path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
   }
 })();
 
-app.use(express.static(path.join(__dirname, "public", "static")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
