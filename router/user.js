f985b961358b4b69f723f7463a6352da5af6383a
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-09-08 15:10:12 +0300

    rm mongodb.connect in user.js(router)

--- router/user.js
+++ router/user.js
@@ -1,22 +1,11 @@
-const { env } = require("../env_var");
+
 const express = require("express");
 const router = express.Router();
-const MongoClient = require("mongodb").MongoClient;
-const mongodb = new MongoClient(env.mongo_url);
-
-(async () => {
-  try {
-    await mongodb.connect();
-  } catch (err) {
-    console.log(err);
-  }
-})();
 
 const db = mongodb.db("TelegramUsers");
 const collection = db.collection("users");
 
 router.use(express.json());
-
 
 router.get(`/user`, async (req, res) => {
   try {
