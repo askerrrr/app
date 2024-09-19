7b683b003542a2f9a5dbf1f98fecb24ac0d8c56b
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-09-19 14:47:54 +0300

    visual changes

--- public/js/services/row/services/checkFileType.js
+++ public/js/services/row/services/checkFileType.js
@@ -2,9 +2,9 @@ export default function checkFileType(file) {
   const image = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(file);
   const document = /\.(xlsx)$/.test(file);
   if (image) {
-    return "it is image";
+    return "image";
   } else if (document) {
-    return "it is doc";
+    return "file.xlsx";
   } else {
     return "Тип файла не определен";
   }
