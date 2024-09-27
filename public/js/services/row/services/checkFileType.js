856d07e88947e4a98543f94ae7cde66ace574434
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-09-27 15:02:57 +0300

    update checkfiletype

--- public/js/services/row/services/checkFileType.js
+++ public/js/services/row/services/checkFileType.js
@@ -1,10 +1,10 @@
 function checkFileType(file) {
   const image = /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(file);
-  const document = /\.(xlsx)$/.test(file);
+  const document = file.startsWith("https://docs.google.com");
   if (image) {
     return "image";
   } else if (document) {
-    return "file.xlsx";
+    return "https://docs.google.com";
   } else {
     return "Тип файла не определен";
   }
