bd0c3cfde3867a925e5d62379ff80f1ee24efd75
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-10-05 17:42:43 +0300

    change checkFileUrl

--- router/services/checkFileUrl.js
+++ router/services/checkFileUrl.js
@@ -1,7 +1,7 @@
 async function checkFileUrl(id, fileURL, collection) {
   return await collection.findOne({
     "orders.orderContent.file.url": fileURL,
-    "orders.tgId": id,
+    tgId: id,
   });
 }
 
