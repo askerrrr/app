66819194d8a504a3e5af83c98ce580ab96dab3ec
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-10-11 11:50:39 +0300

    corrected download path

--- router/downloadFile.js
+++ router/downloadFile.js
@@ -6,15 +6,12 @@ router.get("/var/www/userFiles/:id/:fileid", async (req, res) => {
   try {
     const userId = req.params.id;
     const fileId = req.params.fileid;
-    return res.download(
-      `/var/www/userFiles/${userId}/${fileId}.xlsx`,
-      (err) => {
-        if (err) {
-          console.log("Ошибка при скачивании файла", err);
-        }
-        console.log("Файл успешно скачан");
+    return res.download(`/var/www/userFiles/${userId}/${fileId}`, (err) => {
+      if (err) {
+        console.log("Ошибка при скачивании файла", err);
       }
-    );
+      console.log("Файл успешно скачан");
+    });
   } catch (err) {
     console.log(err);
   }
