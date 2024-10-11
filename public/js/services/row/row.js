432628aa4b438088be8756076c0dfc74e05c1b01
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-10-11 14:22:09 +0300

    add button for delete order

--- public/js/services/row/row.js
+++ public/js/services/row/row.js
@@ -6,31 +6,37 @@ import buttonBack from "./services/buttonBack.js";
 async function createButtonForDeleteOrder(id) {
   const button = document.createElement("button");
   button.id = id;
+  button.append("Удалить");
 
   button.addEventListener("click", async (e) => {
     e.preventDefault();
+    const table = document.getElementById("table");
     const tbody = document.getElementById(id);
-    return tbody.remove();
+    return table.removeChild(tbody);
   });
+
+  return button;
 }
 
 async function row(order) {
-  const table = document.getElementById("table");
   let id = order.orderContent.file.id;
 
   const tr = document.createElement("tr");
-
   tr.append(getDate(order));
   tr.append(getFile(order));
   tr.append(getPhone(order));
-  await createButtonForDeleteOrder(id);
+
+  const button = await createButtonForDeleteOrder(id);
 
   const tbody = document.createElement("tbody");
   tbody.append(tr);
+  tbody.append(button);
   tbody.id = id;
+
+  const table = document.getElementById("table");
   table.append(tbody);
 
-  const body = document.getElementById("allInformationAboutTheOrder");
+  const body = document.getElementById("orderInfo");
   body.append(buttonBack(order));
   body.append(table);
   return body;
