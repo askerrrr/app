b78e32f5a22903f5c3e7e7617cf1cc7a60bd2a98
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-10-21 21:20:09 +0300

    work on checkbox1

--- public/js/services/row/services/saveOrderStatus.js
+++ public/js/services/row/services/saveOrderStatus.js
@@ -7,7 +7,7 @@ export default async function saveOrderStatus(userId, fileId) {
     const checkboxButton = document.querySelectorAll(
       `input[name=order-status]`
     );
-    console.log(checkboxButton);
+
     let arr = [];
     for (let i = 0; i < checkboxButton.length; i++) {
       let id = checkboxButton[i].id;
@@ -20,12 +20,16 @@ export default async function saveOrderStatus(userId, fileId) {
     let trueCheckedId = arr.filter((elem) => elem.value === status)[0].id;
     console.log(trueCheckedId);
 
-    checkboxButton.forEach((item, index) => {
-      if (item.id !== trueCheckedId) {
-        item.checked = true;
-        item.disabled = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (document.getElementById(i) == trueCheckedId) {
+        return console.log(document.getElementById(i));
       }
-    });
+    }
+
+    // for (let i = 0; i <= trueCheckedId; i++) {
+    //   document.getElementById(i).checked = true;
+    //   document.getElementById(i).disabled = true;
+    // }
   } catch (err) {
     console.log(err);
   }
