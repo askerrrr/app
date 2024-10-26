966cafc283b854d55043b89935baa950d8c27aa4
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-10-26 13:31:27 +0300

    add class CheckBox

--- public/js/services/different/checkBoxForOrderStatus.js
+++ public/js/services/different/checkBoxForOrderStatus.js
@@ -1,126 +1,85 @@
-function Status_In_Processing(id) {
-  const label = document.createElement("label");
-  label.for = id;
+class CheckBox {
+  constructor(checkBoxId, checkBoxValue, divContent) {
+    this.checkBoxId = checkBoxId;
+    this.checkBoxValue = checkBoxValue;
+    this.divContent = divContent;
+  }
+  newCheckBox() {
+    const label = document.createElement("label");
+    label.for = this.checkBoxId;
 
-  const input = document.createElement("input");
-  input.id = id;
-  input.type = "checkbox";
-  input.name = "order-status";
-  input.value = `in-processing`;
+    const input = document.createElement("input");
+    input.id = this.checkBoxId;
+    input.type = "checkbox";
+    input.name = "order-status";
+    input.value = this.checkBoxValue;
 
-  const div = document.createElement("div");
-  div.append("взят в обработку");
+    const div = document.createElement("div");
+    div.append(this.divContent);
 
-  label.append(input, div);
-  return label;
-}
-
-function Status_Purchased(id) {
-  const label = document.createElement("label");
-  label.for = id;
-
-  const input = document.createElement("input");
-  input.id = id;
-  input.type = "checkbox";
-  input.name = "order-status";
-  input.value = `purchased`;
-
-  const div = document.createElement("div");
-  div.append("выкуплен");
-
-  label.append(input, div);
-  return label;
-}
-
-function Status_China_Warehouse(id) {
-  const label = document.createElement("label");
-  label.for = id;
-
-  const input = document.createElement("input");
-  input.id = id;
-  input.type = "checkbox";
-  input.name = "order-status";
-  input.value = `china-warehouse`;
-
-  const div = document.createElement("div");
-  div.append("доставлен на склад в китае");
-
-  label.append(input, div);
-  return label;
-}
-
-function Status_On_The_Way(id) {
-  const label = document.createElement("label");
-  label.for = id;
-
-  const input = document.createElement("input");
-  input.id = id;
-  input.type = "checkbox";
-  input.name = "order-status";
-  input.value = `on-the-way`;
-
-  const div = document.createElement("div");
-  div.append("товар в пути");
-
-  label.append(input, div);
-  return label;
-}
-
-function Status_Awaiting_Receipt(id) {
-  const label = document.createElement("label");
-  label.for = id;
-
-  const input = document.createElement("input");
-  input.id = id;
-  input.type = "checkbox";
-  input.name = "order-status";
-  input.value = `awaiting-receipt`;
-
-  const div = document.createElement("div");
-  div.append("ожидает получения");
-
-  label.append(input, div);
-  return label;
-}
-
-function Status_Order_Is_Completed(id) {
-  const label = document.createElement("label");
-  label.for = id;
-
-  const input = document.createElement("input");
-  input.id = id;
-  input.type = "checkbox";
-  input.name = "order-status";
-  input.value = `order-is-completed`;
-
-  const div = document.createElement("div");
-  div.append("заказ завершен");
-
-  label.append(input, div);
-  return label;
+    label.append(input, div);
+    return label;
+  }
 }
 
 export default async function createCheckBoxForOrderStatus(id) {
+  const Status_In_Processing = new CheckBox(
+    id,
+    "in-processing",
+    "взят в обработку"
+  ).newCheckBox();
+
+  const Status_Purchased = new CheckBox(
+    id + 1,
+    "purchased",
+    "выкуплен"
+  ).newCheckBox();
+
+  const Status_China_Warehouse = new CheckBox(
+    id + 2,
+    "china-warehouse",
+    'доставлен на склад в китае"'
+  ).newCheckBox();
+
+  const Status_On_The_Way = new CheckBox(
+    id + 3,
+    "on-the-way",
+    "товар в пути"
+  ).newCheckBox();
+
+  const Status_Awaiting_Receipt = new CheckBox(
+    id + 4,
+    "awaiting-receipt",
+    "ожидает получения"
+  ).newCheckBox();
+
+  const Status_Order_Is_Completed = new CheckBox(
+    id + 5,
+    "order-is-completed",
+    "заказ завершен"
+  ).newCheckBox();
+
   const form = document.getElementById("set-order-status");
 
   const fieldset = document.createElement("fieldset");
   fieldset.id = "fieldset";
-
   const legend = document.createElement("legend");
-  legend.append("Статус заказа");
 
   fieldset.append(
     legend,
-    Status_In_Processing(id),
-    Status_Purchased(id + 1),
-    Status_China_Warehouse(id + 2),
-    Status_On_The_Way(id + 3),
-    Status_Awaiting_Receipt(id + 4),
-    Status_Order_Is_Completed(id + 5)
+    Status_In_Processing,
+    Status_Purchased,
+    Status_China_Warehouse,
+    Status_On_The_Way,
+    Status_Awaiting_Receipt,
+    Status_Order_Is_Completed
   );
+
+  legend.append("Статус заказа");
 
   const childTeg = document.getElementById("submit-order-status");
 
   form.insertBefore(fieldset, childTeg);
+
   return form;
-}
+}//export to public/js/services/different/formForOpenPopUp.js
