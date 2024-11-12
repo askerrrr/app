2c466035ba2c836cfd19ea50f19c8ce814721d8a
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-12 21:52:19 +0300

    work on make dir

--- router/services/different/getUserDir.js
+++ router/services/different/getUserDir.js
@@ -1,8 +1,20 @@
 import fs from "fs";
 import path from "path";
+import { mkdir } from "fs";
 
 export default async function getUserDir(id) {
   const userDir = path.join("/var", "www", "userFiles", `${id}`);
+  const dirNames = ["docs", "images"];
+
+  try {
+    if (!fs.existsSync(userDir)) {
+      await Promise.all(
+        dirNames.map((dirName) => mkdir(`${userDir}/${dirName}`))
+      ).catch((err) => console.log(err));
+    }
+  } catch (err) {
+    console.log(err);
+  }
 
   if (!fs.existsSync(userDir)) {
     await fs.promises.mkdir(userDir, { recursive: true }, (err) => {
