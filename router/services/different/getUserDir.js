6f768f704d989b9f3d786c74adf0fe6198c0c2ce
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-12 20:43:17 +0300

    visual changes

--- router/services/different/getUserDir.js
+++ router/services/different/getUserDir.js
@@ -2,18 +2,20 @@ import fs from "fs";
 import path from "path";
 
 export default async function getUserDir(id) {
-  try {
-    const userDir = path.join("/var", "www", "userFiles", `${id}`);
+  const userDir = path.join("/var", "www", "userFiles", `${id}`);
 
-    if (!fs.existsSync(userDir)) {
-      await fs.promises.mkdir(userDir, { recursive: true });
+  if (!fs.existsSync(userDir)) {
+    await fs.promises.mkdir(userDir, { recursive: true }, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
       console.log(`Папка ${id} успешно создана...`);
-    }
-
-    console.log(`Папка ${id} уже существует...`);
-
-    return userDir;
-  } catch (err) {
-    console.log(`Ошибка при создании папки ${id}`, err);
+    });
   }
+
+  console.log(`Папка ${id} уже существует...`);
+
+  return userDir;
 }
