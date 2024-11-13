3c1ef88b5f01ac8625fdabb6f89487ae1eb48e2a
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-13 12:49:04 +0300

    add try catch

--- router/services/different/getUserDir.js
+++ router/services/different/getUserDir.js
@@ -4,30 +4,34 @@ import path from "path";
 export default async function getUserDir(id) {
   const userDir = path.join("/var", "www", "userFiles", `${id}`);
   const dirNames = ["docs", "images"];
+  try {
+    if (!fs.existsSync(userDir)) {
+      const createdDir = await fs.promises.mkdir(userDir, { recursive: true });
 
-  if (!fs.existsSync(userDir)) {
-    const createdDir = await fs.promises.mkdir(userDir, { recursive: true });
+      if (!createdDir) {
+        console.log(`Ошибка при создании папки ${userDir}`);
+      }
 
-    if (!createdDir) {
-      console.log(`Ошибка при создании папки ${userDir}`);
+      const result = await Promise.all(
+        dirNames.map((dirName) => {
+          if (!fs.existsSync(path.join(userDir, dirName))) {
+            return fs.promises.mkdir(
+              `${userDir}/${dirName}`,
+              { recursive: true },
+              (err) =>
+                console.log(`Ошибка при создании папок docs и images`, err)
+            );
+          }
+        })
+      );
+
+      return result;
     }
 
-    const result = await Promise.all(
-      dirNames.map((dirName) => {
-        if (!fs.existsSync(path.join(userDir, dirName))) {
-          return fs.promises.mkdir(
-            `${userDir}/${dirName}`,
-            { recursive: true },
-            (err) => console.log(`Ошибка при создании папок docs и images`, err)
-          );
-        }
-      })
-    );
+    console.log(`Папка ${id} уже существует...`);
 
-    return result;
+    return dirNames.map((dirName) => path.join(userDir, dirName));
+  } catch (err) {
+    console.log(err);
   }
-
-  console.log(`Папка ${id} уже существует...`);
-
-  return dirNames.map((dirName) => path.join(userDir, dirName));
 }
