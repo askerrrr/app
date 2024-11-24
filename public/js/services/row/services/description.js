4408e5ed343504b9483193e0f045e7483b4e1ea2
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-11-24 17:11:05 +0300

    add div for description

--- public/js/services/row/services/description.js
+++ public/js/services/row/services/description.js
@@ -1,8 +1,13 @@
 export default function renderDescription(description) {
   const td = document.createElement("td");
-  td.append(
-    `Количество : ${description.quantity}\nРазмер : ${description.size}`
-  );
+
+  const quantity = document.createElement("div");
+  const size = document.createElement("div");
+
+  quantity.append("Количество: " + description.quantity);
+  size.append("Размер: " + description.size);
+
+  td.append(quantity, size);
 
   return td;
 }
