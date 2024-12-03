66d11e61696fd21dc38a4a84b72af0900ba4bfed
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-03 22:41:01 +0300

    work on xlsx cell data

--- router/services/different/getDataFromXLSX.js
+++ router/services/different/getDataFromXLSX.js
@@ -1,3 +1,46 @@
+import JSZip from "jszip";
+import { readFile, writeFile } from "fs/promises";
+
+export default async (filePath) => {
+  try {
+    var fileData = await readFile(filePath);
+
+    var zip = await JSZip.loadAsync(fileData);
+
+    var data = Object.keys(zip.files).filter((fileName) =>
+      fileName.startsWith("xl/worksheets/sheet1.xml")
+    );
+
+    var result = await Promise.all(
+      data.map((file) => zip.files[file].async("nodebuffer"))
+    );
+
+    return result.toString().split(" ");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//data("file.xlsx").then((data) => writeFile("text.txt", data, { flag: "w" }));
+
+[
+  "[Content_Types].xml",
+  "_rels/.rels",
+  "xl/workbook.xml",
+  "xl/_rels/workbook.xml.rels",
+  "xl/worksheets/sheet1.xml",
+  "xl/theme/theme1.xml",
+  "xl/styles.xml",
+  "xl/sharedStrings.xml",
+  "xl/drawings/drawing1.xml",
+  "xl/media/image1.jpeg",
+  "xl/media/image2.jpeg",
+  "xl/media/image3.jpeg",
+  "xl/worksheets/_rels/sheet1.xml.rels",
+  "xl/drawings/_rels/drawing1.xml.rels",
+  "docProps/core.xml",
+  "docProps/app.xml",
+];
 //import Exceljs from "exceljs";
 
 // export default async function getDataFromXLSX(filePath) {
--- router/xlsx.js
+++ router/xlsx.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import db from "./services/database/db.js";
+import getDataFromXLSX from "./services/different/getDataFromXLSX.js";
 import getImageFromXLSX from "./services/different/getImageFromXLSX.js";
 
 const router = Router();
@@ -20,12 +21,6 @@ router.get("/", async (_, res) => {
 router.get("/api", async (req, res) => {
   //:userId/:orderId
   try {
-    // const userId = req.params.userId;
-    // const orderId = req.params.orderId;
-    // const collection = req.app.locals.collection;
-
-    // const filePath = await db.findFilePath(userId, orderId, collection);
-    //await getImageFromXLSX("file.xlsx");
     var filePath = "file.xlsx";
     const base64 = await getImageFromXLSX(filePath);
 
@@ -37,4 +32,16 @@ router.get("/api", async (req, res) => {
   }
 });
 
+router.get("/data", async (req, res) => {
+  var filePath = "file.xlsx";
+  var data = await getDataFromXLSX(filePath);
+  return res.json(data);
+});
+
 export { router as xlsx };
+// const userId = req.params.userId;
+// const orderId = req.params.orderId;
+// const collection = req.app.locals.collection;
+
+// const filePath = await db.findFilePath(userId, orderId, collection);
+//await getImageFromXLSX("file.xlsx");
