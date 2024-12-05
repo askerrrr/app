7b13fe8f9e29bef35816f32a9408363fe6394711
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-05 13:43:27 +0300

    add pathParts

--- public/js/xlsx.js
+++ public/js/xlsx.js
@@ -2,6 +2,11 @@ import rowForXLSX from "./services/row/rowForXLSX.js";
 
 async function createXLSX() {
   try {
+    var pathParts = window.location.pathname.split("/");
+    var userId = pathParts[2];
+    var orderId = pathParts[3];
+
+    console.log(pathParts);
     var response = await fetch(`/xlsx/api/${userId}/${orderId}`, {
       method: "GET",
       headers: { Accept: "application/json" },
