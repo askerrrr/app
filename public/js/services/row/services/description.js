abe6599cbc21b445f0523323df5eab875d4d2e7f
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-18 20:12:58 +0300

    quantity => qty

--- public/js/services/row/services/description.js
+++ public/js/services/row/services/description.js
@@ -2,7 +2,7 @@ export default function renderDescription(description) {
   var qty = document.createElement("div");
   var size = document.createElement("div");
 
-  qty.append("Количество: " + description.quantity);
+  qty.append("Количество: " + description.qty);
   size.append("Размер: " + description.size);
 
   var td = document.createElement("td");
