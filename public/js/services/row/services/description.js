a5ead3c92b1804a63de5bf87bca3e247f0812103
Asker Sultanov <asker_sultanov1999@mail.ru>
2024-12-20 21:46:26 +0300

    refactor description && add hr

--- public/js/services/row/services/description.js
+++ public/js/services/row/services/description.js
@@ -2,11 +2,12 @@ export default function renderDescription(description) {
   var qty = document.createElement("div");
   var size = document.createElement("div");
 
-  qty.append("Количество: " + description.qty);
-  size.append("Размер: " + description.size);
+  qty.append("Количество : " + description.qty);
+  size.append("Размер : " + description.size);
 
+  var hr = document.createElement("hr");
   var td = document.createElement("td");
-  td.append(qty, size);
+  td.append(qty, hr, size);
 
   return td;
 }
