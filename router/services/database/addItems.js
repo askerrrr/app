077466bc2236f56fbf8397e7a1eeada352b5dc90
asker <askersultanov666@gmail.com>
2024-12-31 12:30:24 +0300

    rm log

--- router/services/database/addItems.js
+++ router/services/database/addItems.js
@@ -4,7 +4,6 @@ export default async (userId, orderId, xlsxData, collection) => {
 
     var items = url.map((item) => item + ":::" + 0);
 
-    console.log(items);
     return await collection.updateOne(
       { userId, "orders.order.id": orderId },
       {
