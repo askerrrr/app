a53ccc71b6b9b2b1bb767e557e0a8ed0a34009df
asker <askersultanov666@gmail.com>
2025-02-15 10:57:18 +0300

    refactoring init-mongo.js

--- init-mongo.js
+++ init-mongo.js
@@ -3,15 +3,15 @@ db.getSiblingDB("admin").auth(
   process.env.MONGO_INITDB_ROOT_PASSWORD
 );
 
-db.createUser({
-  user: process.env.MONGO_USER,
-  pwd: process.env.MONGO_PASSWORD,
-  roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"],
+db.getSiblingDB("admin").createUser({
+  user: process.env.MONGO_ROOT_USER,
+  pwd: process.env.MONGO_ROOT_PASSWD,
+  roles: ["userAdminAnyDatabase", "readWriteAnyDatabase"],
 });
 
-db.createCollection("adminData");
+db.getSiblingDB("admin").createCollection("adminData");
 
-db.adminData.insertOne({
+db.getSiblingDB("admin").adminData.insertOne({
   login: process.env.HASH_LOGIN,
   passwd: process.env.HASH_PASSWD,
 });
