0634d7b9b8ff48fc08db2c02b9962013476c498b
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-03-06 13:48:17 +0300

    update getOrderDetailsForBot

--- src/router/botApi/services/getOrderDetailsForBot.js
+++ src/router/botApi/services/getOrderDetailsForBot.js
@@ -7,6 +7,9 @@ var getOrderDetailsForBot = async (data) => {
       id: data.orders[i].order.id,
       date: data.orders[i].order.date,
       phone: data.orders[i].order.phone,
+      type: data.orders[i].order.type,
+      userName: data.orders[i].order.userName,
+      firstName: data.orders[i].order.firstName,
       orderStatus: data.orders[i].order.orderStatus,
     });
   }
