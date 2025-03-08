8d4dc0b9c483057b1ce99428b53a3ec0645b744f
asker <askersultanov666@gmail.com>
2025-03-08 17:19:27 +0300

    update deleteOrderFile

--- src/router/userOrder/services/deleteOrderFile.js
+++ src/router/userOrder/services/deleteOrderFile.js
@@ -1,18 +1,18 @@
 import { rm } from "fs/promises";
 import db from "../../../database/db.js";
-import checkFileExists from "../../xlsx/services/checkFileExists.js";
 
 var deleteOrderFile = async (userId, fileId, collection) => {
+  var filePath = await db.findFilePath(userId, fileId, collection);
+
   try {
-    var filePath = await db.findFilePath(userId, fileId, collection);
-
     await rm(filePath);
-
-    var fileIsExists = await checkFileExists(filePath);
-
-    return fileIsExists == false ? true : false;
+    return true;
   } catch (err) {
-    console.log(err);
+    if (err.code == "ENOENT") {
+      return false;
+    } else {
+      throw err;
+    }
   }
 };
 
